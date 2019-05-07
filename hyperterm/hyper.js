ec673aeefe98932752b4cd6012eb9d6c154e7751
Sunny Golovine <sunny@glvn.co>
2019-05-06 21:43:36 -0400

    updated hyper theme

--- hyperterm/hyper.js
+++ hyperterm/hyper.js
@@ -1,7 +1,3 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
   config: {
     updateChannel: "canary",
@@ -13,14 +9,12 @@ module.exports = {
     cursorBlink: true,
     showHamburgerMenu: "true",
     showWindowControls: "true",
-    padding: "2.5px",
+    padding: "1px",
     shell: "/usr/bin/zsh",
-    wickedBorder: true,
-    wickedBorderColor: "#fff",
     copyOnSelect: true
   },
 
-  plugins: ["hyperterm-cobalt2-theme"],
+  plugins: ["hyper-seti"],
 
   localPlugins: [],
 
