f8c546f1e3cf9db63cec7b688bf09bb8e1526bc6
Sunny Golovine <sunny@glvn.co>
2021-09-14 19:27:11 -0400

    Added toold bin path for android

--- zsh/modules/android.zsh
+++ zsh/modules/android.zsh
@@ -3,5 +3,6 @@ export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 # Tools
 export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
 # Emulator
 export PATH=$PATH:$ANDROID_HOME/emulator
