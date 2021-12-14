4ff51abdd717d09ab2d39f45d95f8b332f9da8d8
Sunny Golovine <sunny@glvn.co>
2021-12-14 10:03:49 -0700

    Add RVM, NVM loading to path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,14 @@
 # Add things to your path
-# Path Exports
+
+
+## GNU CoreUtils
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+## Node Version Manager
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+
+## Ruby Version Manager
+export PATH="$PATH:$HOME/.rvm/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
