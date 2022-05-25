09d291948fa555429de6c76f9103ac09aa36c241
Sunny Golovine <sunny@glvn.co>
2022-05-25 07:41:37 -0600

    Update path.zsh for rbenv

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,11 +4,13 @@
 ## GNU CoreUtils
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
+## Azul JDK 11
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
+
+
 # Node Version Manager
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-
-## Ruby Version Manager
-export PATH="$PATH:$HOME/.rvm/bin"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# RBENV Initialization
+eval "$(rbenv init - zsh)"
