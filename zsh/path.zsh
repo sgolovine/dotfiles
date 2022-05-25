a7c1529675976aca3f031966e3ebd4d6fd55885e
Sunny Golovine <sunny@glvn.co>
2022-05-25 07:59:42 -0600

    Update RBENV config

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -11,6 +11,3 @@ export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
 # Node Version Manager
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-# RBENV Initialization
-eval "$(rbenv init - zsh)"
---
+++ zsh/rbenv.zsh
@@ -0,0 +1,8 @@
+## Source RBENV
+eval "$(rbenv init - zsh)"
+
+## Source Shims
+export PATH="$HOME/.rbenv/shims:$PATH"
+
+## Source Autocompletion
+export PATH="$HOME/.rbenv/completions/rbenv.zsh:$PATH"
