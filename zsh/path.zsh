577e75727a2a2ae42dda8bed05d34ece32d64fb2
Sunny Golovine <sunny@glvn.co>
2023-02-07 10:32:13 -0700

    setup autoremote

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,7 +2,7 @@
 
 
 ## GNU CoreUtils
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$HOME/.cargo/bin:$PATH"
 
 ## Azul JDK 11
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
