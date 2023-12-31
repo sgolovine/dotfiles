3c816929fc735c19deff8eefed33dbc1d6d126d8
Avery, Sunny <Sunny.Avery@elevancehealth.com>
2023-12-31 09:59:47 -0500

    remove some old stuff

--- zsh/rbenv.zsh
+++
@@ -1,8 +0,0 @@
-## Source RBENV
-eval "$(rbenv init - zsh)"
-
-## Source Shims
-export PATH="$HOME/.rbenv/shims:$PATH"
-
-## Source Autocompletion
-export PATH="$HOME/.rbenv/completions/rbenv.zsh:$PATH"
