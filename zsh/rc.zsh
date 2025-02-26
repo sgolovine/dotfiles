f643f2c4519a651ddd88a33f886af52e45daf09b
Sunny Golovine <sunny@golovinemail.org>
2025-02-26 13:22:46 -0700

    feat: add cursor alias

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -8,6 +8,10 @@ source $ZSH/oh-my-zsh.sh
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+# cursor
+alias cursor='/opt/cursor/cursor.appimage --no-sandbox'
+
+
 # git aliases
 alias ga='git add -A'
 alias gd='git diff'
