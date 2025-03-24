a7ef2b39f8964fc9bf4226499ff706053538b344
Sunny Golovine <sunny@golovinemail.org>
2025-03-24 10:19:49 -0600

    feat: update zsh theme

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -5,11 +5,17 @@ export PATH="$PATH:/home/sgolovine/.local/bin"
 export PATH="$PATH:$DOTFILES/bin"
 export NVM_DIR="$HOME/.nvm"
 export EDITOR=nvim
+export DEFAULT_USER=sgolovine
 
 # zshrc
-ZSH_THEME="robbyrussell"
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
+
+# oh-my-zsh theme
+# DEFAULT_USER is required so that the shell
+# does not show the username in the powerline
+ZSH_THEME="agnoster"
+DEFAULT_USER=sgolovine
 
 # node version manager
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
